15d50c18db4db31ffbae9317e3f1f3a7cc1bc1ee
Anushka Chougule <102580839+Anushka23ja@users.noreply.github.com>
2024-06-13 15:58:51 -0700

    Add files via upload

---
+++ Day6/demo1.js
@@ -0,0 +1,18 @@
+//Function Expression 
+var myfun1 = function show(){
+    console.log("It is a function Expression");
+}
+
+//Anonymous Function
+var myfun2 = function(){
+    console.log("It is a Anonymous Functions");
+}
+
+//Arrow Function
+var myfun3 = () => {
+    console.log("It is a Arrow Function");
+}
+
+myfun1();
+myfun2();
+myfun3();
---
+++ Day6/demo3.js
@@ -0,0 +1,10 @@
+//Traditional Function Expression
+const square = function(x){
+    return x * x;
+};
+
+//Arrow Function Equivalent 
+const squareArrow = x => x * x;
+
+console.log(square(4));         //Output: 16
+console.log(squareArrow(4));    //Output: 16
